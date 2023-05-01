85f7683bf438741485fe01799d2a338ad9659876
christaine <christaine.miranda@gmail.com>
2023-05-01 18:15:03 -0300

    fix: name pattern in directories booking

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { authenticateToken, validateBody } from '@/middlewares';
 import { bookingSchema } from '@/schemas/booking-schemas';
-import { createBooking, getBooking, updateBooking } from '@/controllers/bookings-controller';
+import { createBooking, getBooking, updateBooking } from '@/controllers/booking-controller';
 
 const bookingRouter = Router();
 bookingRouter
